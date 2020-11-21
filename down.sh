3d4f18abc9dea231e2f8cc063b4b2824b2f4becf
SayanGhoshBDA <67162603+SayanGhoshBDA@users.noreply.github.com>
2020-11-21 22:29:58 +0530

    Update down.sh

--- down.sh
+++ down.sh
@@ -1,7 +1,7 @@
 i=0; 
 for f in *; 
 do 
-    d=product_images_$(printf %03d $((i/20480+1))); 
+    d=product_images_$(printf %03d $((i/20480+1))); # 20480 = file count; 
     mkdir -p $d; 
     mv "$f" $d; 
     let i++; 
