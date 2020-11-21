4a270123be884e8cf7f6e00081ed2a8d3d1c6119
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2020-11-22 00:42:54 +0530

    Update colab_move.sh

--- colab_move.sh
+++ colab_move.sh
@@ -3,5 +3,5 @@
 git clone https://github.com/SayanGhoshBDA/Outputs
 for i in {1..22}
 do
-	unzip -qq "product_images_$(printf %03d $i).zip" 
+	unzip -qq "Outputs/product_images_$(printf %03d $i).zip" 
 done
