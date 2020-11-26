7782e9b5d1dd03d73f9b96a000bb53f17241ccfe
SayanGhoshBDA <67162603+SayanGhoshBDA@users.noreply.github.com>
2020-11-26 18:21:08 +0530

    Update colab_move.sh

--- colab_move.sh
+++ colab_move.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 # Run in colab
 # To unzip and move folders to drive
+%%shell
+
 git clone https://github.com/SayanGhoshBDA/Outputs
 
 cd Outputs
 
-for i in {1..2}
+for i in {1..22}
 do
     unzip -q "./product_images_$(printf %03d $i).zip"
 done
 
-mkdir ../product_images
+mkdir product_images
 
-find -name '*.png' | xargs mv --target-directory=product_images > /dev/null 2>&1
+find . -name '*.png' | xargs mv --target-directory=product_images > /dev/null 2>&1
 
+mv product_images ..
 cd ..
 
 rm -r Outputs
